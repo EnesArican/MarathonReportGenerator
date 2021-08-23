0178fc085ce2926c838e25e2fd36dfac3e65e6a2
Enes Arican <enes.arican@accounttechnologies.com>
2021-08-23 19:21:17 +0100

    adding report form

--- MyApp/src/shared/router.ts
+++ MyApp/src/shared/router.ts
@@ -10,6 +10,7 @@ import SignIn from '../components/SignIn.vue';
 import SignUp from '../components/SignUp.vue';
 import Profile from '../components/Profile.vue';
 import Admin from '../components/Admin/index.vue';
+import ReportForm from '../components/ReportForm.vue';
 
 export enum Routes {
   Home = '/',
@@ -19,40 +20,22 @@ export enum Routes {
   Profile = '/profile',
   Admin = '/admin',
   Forbidden = '/forbidden',
+  ReportForm = '/report-form',
 }
 
 Vue.use(Router);
 
-function requiresAuth(to: Route, from: Route, next: (to?: string) => void) {
-  if (!store.userSession) {
-    next(`${Routes.SignIn}?redirect=${encodeURIComponent(to.path)}`);
-    return;
-  }
-  next();
-}
 
-function requiresRole(role: string) {
-  return (to: Route, from: Route, next: (to?: string) => void) => {
-    if (!store.userSession) {
-      next(`${Routes.SignIn}?redirect=${encodeURIComponent(to.path)}`);
-    }
-    else if (!store.userSession.roles || store.userSession.roles.indexOf(role) < 0) {
-      next(`${Routes.Forbidden}?role=${encodeURIComponent(role)}`);
-    }
-    else {
-      next();
-    }
-  };
-}
 
 const routes = [
   { path: Routes.Home, component: Home, props: { name: 'Vue' } },
   { path: Routes.About, component: About, props: { message: 'About page' } },
   { path: Routes.SignIn, component: SignIn },
   { path: Routes.SignUp, component: SignUp },
-  { path: Routes.Profile, component: Profile, beforeEnter: requiresAuth },
-  { path: Routes.Admin, component: Admin, beforeEnter: requiresRole('Admin') },
+  { path: Routes.Profile, component: Profile },
+  { path: Routes.Admin, component: Admin },
   { path: Routes.Forbidden, component: Forbidden },
+  { path: Routes.ReportForm, component: ReportForm },
   { path: '*', redirect: '/' },
 ];
 
